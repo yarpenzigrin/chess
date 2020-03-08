952a134a7e88870e737a92b3f0270779b117aa4f
Tom Bombadil <temp@temp.temp>
2020-03-08 19:36:04 +0000

    Added -O3 to compilation flags

--- Make/build.sh
+++ Make/build.sh
@@ -3,4 +3,4 @@
 g++ -std=c++14 -ggdb3 -I src test/core.cpp -o build/core_tests
 g++ -std=c++14 -ggdb3 -I src test/gameplay.cpp -o build/gameplay_tests
 g++ -std=c++14 -ggdb3 -I src test/misc.cpp -o build/misc_tests
-g++ -std=c++14 -ggdb3 -I src src/random_game.cpp -o build/random_game
+g++ -std=c++14 -ggdb3 -I src src/random_game.cpp -o build/random_game -O3
