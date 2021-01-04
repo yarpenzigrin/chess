ab457415e6f671c5ec6176a3762a3240121d8ba3
Tom Bombadil <temp@temp.temp>
2021-01-04 20:30:25 +0100

    Basic CMake configuration

--- Make/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-g++ -std=c++14 -ggdb3 -I include test/core.cpp -o build/core_tests
-g++ -std=c++14 -ggdb3 -I include test/gameplay.cpp -o build/gameplay_tests
-g++ -std=c++14 -ggdb3 -I include test/misc.cpp -o build/misc_tests
-g++ -std=c++14 -ggdb3 -I include examples/random_game.cpp -o build/random_game -O3
--- Make/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-build/core_tests
-build/gameplay_tests
-build/misc_tests
